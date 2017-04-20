35630e9577f45c24c3ba0fe4a03c676e01c09505
ksbrar <kabirbrar1@gmail.com>
2017-04-19 19:36:22 -0700

    Added trilateration code

--- app/app/src/main/java/com/example/redcross/app/TrilaterationDemo.java
+++ app/app/src/main/java/com/example/redcross/app/TrilaterationDemo.java
@@ -2,6 +2,9 @@ package com.example.redcross.app;
 
 import android.content.Context;
 
+import org.apache.commons.math3.fitting.leastsquares.LeastSquaresOptimizer.Optimum;
+import org.apache.commons.math3.fitting.leastsquares.LevenbergMarquardtOptimizer;
+
 import android.util.Log;
 
 /**
@@ -18,6 +21,15 @@ public class TrilaterationDemo {
         // Map RSSI values to distances, meters (Abirami)
 
         // Determine position of this device from distances and locations of other devices (Kabir)
+        // fake af test LOL
+        double[][] positions = new double[][] { { 5.0, -6.0 }, { 13.0, -15.0 }, { 21.0, -3.0 }, { 12.4, -21.2 } };
+        double[] distances = new double[] { 8.06, 13.97, 23.32, 15.31 };
+
+        NonLinearLeastSquaresSolver solver = new NonLinearLeastSquaresSolver(new TrilaterationFunction(positions, distances), new LevenbergMarquardtOptimizer());
+        Optimum optimum = solver.solve();
+
+        // the answer
+        double[] centroid = optimum.getPoint().toArray();
 
 
         // Send data to server
