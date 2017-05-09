5fa98b928317014378534a166ff7f721df73b92d
anniewang1914 <Annie@Annies-MacBook-Pro.local>
2017-05-08 20:49:25 -0700

    Basic google maps visual

---
+++ LocationDemo/app/src/main/java/com/example/annie/locationdemo/MapsActivity.java
@@ -0,0 +1,49 @@
+package com.example.annie.locationdemo;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Moore and move the camera
+        LatLng moore = new LatLng(34.139704, -118.124659);
+        LatLng mooreTest = new LatLng(34.139700, -118.124600);
+        mMap.addMarker(new MarkerOptions().position(moore).title("Marker in Moore"));
+        mMap.addMarker(new MarkerOptions().position(mooreTest).title("Data Sample Marker"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(moore));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(19));
+
+    }
+}
